6bdac0692fa657325cffc3d57e3e4c334b1f5234
bneumitij <bneumitij@gmail.com>
2022-09-22 20:14:48 +0300

    ...

---
+++ index.js
@@ -0,0 +1,12 @@
+const a=10;
+const b=2.5;
+// const c=a-b;
+console.log(a / b);
+// alert('Good');
+// const shouldRenew = confirm('dbqlb hjp,sqybre');
+// console.log(shouldRenew);
+// let quantiy = prompt('dbqlb hjp,sqybre');
+// quantiy = prompt(quantiy);
+// console.log(quantiy);
+// console.log(typeof quantiy);
+// console.log(typeof '');
